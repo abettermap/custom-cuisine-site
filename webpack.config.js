743685adfc723763c1e3b3b15319fb9d1a216409
Jason Lampel <jason@bettermap.com>
2018-10-15 19:39:04 -0500

    Load HTML partials

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -13,29 +14,36 @@ module.exports = {
         use: [
           {
             // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
+            loader: "style-loader"
           },
           {
             // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
             // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
+              plugins: function() {
+                return [require("autoprefixer")];
               }
             }
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
+            loader: "sass-loader"
           }
         ]
       }
     ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/home.html',
+  })],
+  devServer: {
+    contentBase: "dist/", // Relative directory for base of server
+    publicPath: "/", // Live-reload
+    inline: true,
+    historyApiFallback: true
   }
 };
